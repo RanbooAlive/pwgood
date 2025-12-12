f85207d32aac35ffb89ccfbecc7a974b9fdbea48
RanbooAlive <kiramirzoeva167@gmail.com>
2025-12-13 01:28:16 +0600

    7.1-7.3b

--- exercises/07_files/task_7_3.py
+++ exercises/07_files/task_7_3.py
@@ -20,3 +20,10 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+with open("CAM_table.txt") as conf:
+    for line in conf:
+        words = line.split()
+        if words and words[0].isdigit():
+            vlan, mac, _, interface = words
+            print(f"{vlan:9}{mac:20}{interface}")
--- exercises/07_files/task_7_3a.py
+++ exercises/07_files/task_7_3a.py
@@ -40,3 +40,14 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+mac_table = []
+
+with open("CAM_table.txt", "r") as conf:
+    for line in conf:
+        words = line.split()
+        if words and words[0].isdigit():
+            vlan, mac, _, intf = words
+            mac_table.append([int(vlan), mac, intf])
+
+for vlan, mac, intf in sorted(mac_table):
+    print(f"{vlan:<9}{mac:20}{intf}")
