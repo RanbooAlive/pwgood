a35efedf6f437fc1c81f60e608e001c2751ca409
RanbooAlive <kiramirzoeva167@gmail.com>
2025-12-18 23:25:53 +0600

    Выполнены задания 11.х

--- exercises/11_modules/task_11_2a.py
+++ exercises/11_modules/task_11_2a.py
@@ -80,3 +80,11 @@ infiles = [
     "sh_cdp_n_r2.txt",
     "sh_cdp_n_r3.txt",
 ]
+
+
+def unique_network_map(topology_dict):
+network_map = {}
+for key, value in topology_dict.items():
+if not network_map.get(value) == key:
+network_map[key] = value
+return network_map
